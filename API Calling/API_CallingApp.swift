c1baa02307c258ef9bee4156feb2129000ba30b1
Alex Artamonov <aartamonov@bsd220.org>
2026-03-19 14:21:52 -0500

    Initial Commit

---
+++ API Calling/API_CallingApp.swift
@@ -0,0 +1,17 @@
+//
+//  API_CallingApp.swift
+//  API Calling
+//
+//  Created by Alex Artamonov on 3/19/26.
+//
+
+import SwiftUI
+
+@main
+struct API_CallingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
